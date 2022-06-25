f15f00c7f5b9b76d1e6f60894801ca3987126256
tjquillan <tjquillan@gmail.com>
2022-06-25 14:24:00 -0700

    Update plugins-bootstrap.zsh
    * Small cleanups
    * Add Docker completion

--- exact_dot_zsh/plugins-bootstrap.zsh
+++ exact_dot_zsh/plugins-bootstrap.zsh
@@ -1,10 +1,10 @@
-local zsh_data_dir="$XDG_DATA_HOME/zsh"
+ZSH_DATA_DIR="$XDG_DATA_HOME/zsh"
 
 declare -A ZI
-ZI[HOME_DIR]="$zsh_data_dir/zi"
+ZI[HOME_DIR]="${ZSH_DATA_DIR}/zi"
 ZI[BIN_DIR]="${ZI[HOME_DIR]}/bin"
 ZI[ZMODULES_DIR]="${ZI[HOME_DIR]}/zmodules"
-ZI[ZCOMPDUMP_PATH]="$zsh_data_dir/zcompdump"
+ZI[ZCOMPDUMP_PATH]="${ZSH_DATA_DIR}/zcompdump"
 
 if [[ ! -f "${ZI[BIN_DIR]}/zi.zsh" ]]; then
     command git clone https://github.com/z-shell/zi.git "${ZI[BIN_DIR]}"
@@ -14,23 +14,25 @@ fi
 
 module_path+=( "${ZI[ZMODULES_DIR]}/zpmod/Src" )
 zmodload zi/zpmod
-source "${ZI[BIN_DIR]}/zi.zsh"
+[[ -z "${ZI_REGISTERED_PLUGINS}" ]] && source "${ZI[BIN_DIR]}/zi.zsh"
 autoload -Uz _zi
 (( ${+_comps} )) && _comps[zi]=_zi
 
 # =============== [Completions] ===============
-zi light-mode lucid wait as"completion" blockf atpull"zi creinstall -q ." for \
+zi light-mode lucid wait as"completion" blockf for \
     zsh-users/zsh-completions \
     gradle/gradle-completion
 
 zi is-snippet lucid wait as"completion" blockf for \
     has"cargo" \
-        https://github.com/rust-lang/cargo/blob/master/src/etc/_cargo
-        
+        https://github.com/rust-lang/cargo/blob/master/src/etc/_cargo \
+    has"docker" \
+        https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker
+
 zi is-snippet lucid wait for \
     has"pyenv" \
         https://github.com/pyenv/pyenv/blob/master/completions/pyenv.zsh
-    
+
 # ================= [Prompt] ==================
 zi light-mode lucid for \
     iboyperson/p9k-theme-pastel \
