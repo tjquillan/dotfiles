41db2e63afcdf59559e3415d2c0a4593e160c7d8
tjquillan <tjquillan@gmail.com>
2022-10-19 15:40:44 -0400

    Make install scripts into templates

--- .chezmoiscripts/run_once_after_poetry-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo -e "\033[0;34m==> Installing Poetry...\033[0m"
-
-if [[ ! -d $HOME/.local/share/pypoetry ]]; then
-    curl -sSL https://install.python-poetry.org | python3 -
-else
-    echo -e "\033[0;33m  -> Poetry is already installed.\033[0m"
-fi
--- .chezmoiscripts/run_once_after_rustup-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo -e "\033[0;34m==> Installing Rustup...\033[0m"
-
-if [[ ! -d $RUSTUP_HOME ]]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | zsh -s -- --no-modify-path -y
-else
-    echo -e "\033[0;33m  -> Rustup is already installed.\033[0m"
-fi
