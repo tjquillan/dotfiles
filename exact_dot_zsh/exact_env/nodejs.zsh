2022c924de9192d5ca2b095110834846a8652fd3
tjquillan <tjquillan@gmail.com>
2022-11-26 16:29:30 -0500

    Move nodejs to xdg base directory

--- exact_dot_zsh/exact_env/nodejs.zsh
+++ exact_dot_zsh/exact_env/nodejs.zsh
@@ -1,6 +1,7 @@
 ##### XDG Base Directory Specification Compliance #####
 # Node.js
 export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/config
 
 #### Path ####
 export PATH="$XDG_DATA_HOME/yarn/global/node_modules/.bin:$PATH"
