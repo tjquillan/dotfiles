0923385c9a052446658683363af101aa34e2d880
tjquillan <tjquillan@gmail.com>
2022-12-11 16:26:44 -0500

    Rework gnome_keyring SSH_AUTH_SOCK handling

--- exact_dot_zsh/exact_env/gnome_keyring.zsh
+++ exact_dot_zsh/exact_env/gnome_keyring.zsh
@@ -1,4 +1,7 @@
-if [[ ! -v SSH_AUTH_SOCK ]]; then
+GCR_SSH_AUTH_SOCK="${XDG_RUNTIME_DIR:-/run/user/$UID}"/gcr/ssh
+if [[ -z SSH_AUTH_SOCK && -e "${GCR_SSH_AUTH_SOCK}" ]]; then
+    export SSH_AUTH_SOCK="${GCR_SSH_AUTH_SOCK}"
+elif [[ -z SSH_AUTH_SOCK ]]; then
     eval $(gnome-keyring-daemon --start 2> /dev/null)
     export SSH_AUTH_SOCK
 fi
