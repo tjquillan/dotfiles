43bf965e1db73cb7a2c7cb2b9a4cf56b20c15491
tjquillan <tjquillan@gmail.com>
2022-12-13 10:03:51 -0500

    Add Volta support

--- exact_dot_zsh/exact_env/nodejs.zsh
+++ exact_dot_zsh/exact_env/nodejs.zsh
@@ -2,6 +2,11 @@
 # Node.js
 export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/config
+# Volta (https://volta.sh/)
+export VOLTA_HOME="$XDG_DATA_HOME"/volta
 
 #### Path ####
+# Yarn
 export PATH="$XDG_DATA_HOME/yarn/global/node_modules/.bin:$PATH"
+# Volta
+export PATH="$VOLTA_HOME/bin:$PATH"
