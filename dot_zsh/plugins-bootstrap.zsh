765eba32f4295ada15a88bf651b38e0c6ea3dc1b
tjquillan <tjquillan@gmail.com>
2023-06-14 18:30:23 -0700

    Switch plugin manager to Antidote

---
+++ dot_zsh/plugins-bootstrap.zsh
@@ -0,0 +1,16 @@
+# Set the name of the static .zsh plugins file antidote will generate.
+zsh_plugins=${ZDOTDIR:-~}/.zsh_plugins.zsh
+
+zstyle ':antidote:bundle' use-friendly-names 'yes'
+
+# Lazy-load antidote.
+fpath+=(${ZDOTDIR:-~}/.antidote)
+autoload -Uz $fpath[-1]/antidote
+
+# Generate static file in a subshell when .zsh_plugins.txt is updated.
+if [[ ! $zsh_plugins -nt ${zsh_plugins:r}.txt ]]; then
+  (antidote bundle <${zsh_plugins:r}.txt >|$zsh_plugins)
+fi
+
+# Source your static plugins file.
+source $zsh_plugins
