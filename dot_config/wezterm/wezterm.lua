0d9891e83ded875abc96262ea12ecfa4f9eaf36c
tjquillan <tjquillan@gmail.com>
2024-01-30 11:59:08 -0700

    Update wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,31 +3,33 @@ local mux = wezterm.mux
 
 -- Manimize on startup
 wezterm.on("gui-startup", function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
 end)
 
 -- Tab title
-wezterm.on(
-  "format-tab-title",
-  function(tab, tabs, panes, config, hover, max_width)
-    local pane = tab.active_pane
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local pane = tab.active_pane
 
-    local dir_name = pane.current_working_dir
-    dir_name = (dir_name:find(wezterm.home_dir .. "/?$") and {"~"} or {dir_name:gsub(".*[/\\]([^/\\]+)[/\\]?", "%1")})[1]
+	local dir_name = pane.current_working_dir.file_path
+	dir_name = (
+		dir_name:find(wezterm.home_dir .. "/?$") and { "~" } or { dir_name:gsub(".*[/\\]([^/\\]+)[/\\]?", "%1") }
+	)[1]
 
-    local ps_name = pane.foreground_process_name:gsub("(.*[/\\])(.*)", "%2")
+	local ps_name = pane.foreground_process_name:gsub("(.*[/\\])(.*)", "%2")
 
-    local title = dir_name .. " : " .. ps_name
+	local title = dir_name .. " : " .. ps_name
 
-    return {
-      { Text = title }
-    }
-  end
-)
+	return {
+		{ Text = title },
+	}
+end)
 
 -- This table will hold the configuration.
 local config = {}
+
+-- Set the GPU Front End
+config.front_end = "WebGpu"
 
 -- Set color scheme
 config.color_scheme = "OneHalfDark"
@@ -36,12 +38,11 @@ local color_scheme = wezterm.color.get_builtin_schemes()[config.color_scheme]
 -- Don't check for updates
 config.check_for_updates = false
 
-
 -- Set the font
 config.font = wezterm.font_with_fallback({
-  "Fira Code",
-  { family = "Symbols Nerd Font Mono" },
-  { family = "JoyPixels", assume_emoji_presentation = true },
+	"Fira Code",
+	{ family = "Symbols Nerd Font Mono" },
+	{ family = "JoyPixels", assume_emoji_presentation = true },
 })
 config.font_size = 12.0
 
@@ -49,39 +50,44 @@ config.font_size = 12.0
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tab = false
 config.window_frame = {
-  font = wezterm.font_with_fallback({
-    { family = "Roboto", weight = "Bold" },
-    { family = "JoyPixels", assume_emoji_presentation = true },
-  }),
-  font_size = 12.0,
-  active_titlebar_bg = color_scheme.background,
-  inactive_titlebar_bg = color_scheme.background
+	font = wezterm.font_with_fallback({
+		{ family = "Roboto", weight = "Bold" },
+		{ family = "JoyPixels", assume_emoji_presentation = true },
+	}),
+	font_size = 12.0,
+	active_titlebar_bg = color_scheme.background,
+	inactive_titlebar_bg = color_scheme.background,
 }
 config.colors = {
-  tab_bar = {
-    inactive_tab_edge = wezterm.color.parse(color_scheme.background):lighten(0.05),
-    active_tab = {
-      bg_color = wezterm.color.parse(color_scheme.background):lighten(0.1),
-      fg_color = color_scheme.foreground,
-    },
-    inactive_tab = {
-      bg_color = wezterm.color.parse(color_scheme.background):lighten(0.05),
-      fg_color = color_scheme.foreground,
-    },
-    inactive_tab_hover = {
-      bg_color = wezterm.color.parse(color_scheme.background):lighten(0.08),
-      fg_color = color_scheme.foreground,
-    },
-    new_tab = {
-      bg_color = color_scheme.background,
-      fg_color = color_scheme.foreground,
-    },
-    new_tab_hover = {
-      bg_color = color_scheme.background,
-      fg_color = color_scheme.ansi[3],
-    }
-  }
+	tab_bar = {
+		inactive_tab_edge = wezterm.color.parse(color_scheme.background):lighten(0.05),
+		active_tab = {
+			bg_color = wezterm.color.parse(color_scheme.background):lighten(0.1),
+			fg_color = color_scheme.foreground,
+		},
+		inactive_tab = {
+			bg_color = wezterm.color.parse(color_scheme.background):lighten(0.05),
+			fg_color = color_scheme.foreground,
+		},
+		inactive_tab_hover = {
+			bg_color = wezterm.color.parse(color_scheme.background):lighten(0.08),
+			fg_color = color_scheme.foreground,
+		},
+		new_tab = {
+			bg_color = color_scheme.background,
+			fg_color = color_scheme.foreground,
+		},
+		new_tab_hover = {
+			bg_color = color_scheme.background,
+			fg_color = color_scheme.ansi[3],
+		},
+	},
 }
+
+-- Stylize the cursor
+config.default_cursor_style = "SteadyBar"
 
 return config
