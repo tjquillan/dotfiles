e0231d818708923dfec566b895418396673a7ef8
tjquillan <tjquillan@gmail.com>
2024-01-30 11:58:44 -0700

    Remove yarn global modules

--- dot_zsh/exact_env/nodejs.zsh
+++ dot_zsh/exact_env/nodejs.zsh
@@ -6,7 +6,5 @@ export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/config
 export VOLTA_HOME="$XDG_DATA_HOME"/volta
 
 #### Path ####
-# Yarn
-export PATH="$XDG_DATA_HOME/yarn/global/node_modules/.bin:$PATH"
 # Volta
 export PATH="$VOLTA_HOME/bin:$PATH"
