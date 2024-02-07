d6049e03d98b6050404ef1f69fdeaad28351abd0
tjquillan <tjquillan@gmail.com>
2024-02-07 14:31:44 -0700

    Move fvm to more general dart

--- dot_zsh/exact_env/dart.zsh
+++ dot_zsh/exact_env/dart.zsh
@@ -1,5 +1,8 @@
 ##### XDG Base Directory Specification Compliance #####
+# Fvm (https://fvm.app)
 export FVM_HOME="$XDG_DATA_HOME/fvm"
+# Pub (https://dart.dev/tools/pub/environment-variables)
+export PUB_CACHE="$XDG_CACHE_HOME/pub"
 
 ##### Path #####
 export PATH="$FVM_HOME/default/bin:$PATH"
