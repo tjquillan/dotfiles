f35c9a5447ab47c4d2ae42c51b98a2142f3b82d9
tjquillan <tjquillan@gmail.com>
2025-02-22 13:47:55 -0700

    Add Ruby XDG vars

---
+++ home/dot_zsh/exact_env/ruby.zsh
@@ -0,0 +1,5 @@
+##### XDG Base Directory Specification Compliance #####
+# Ruby
+export BUNDLE_USER_CACHE=$XDG_CACHE_HOME/bundle
+export BUNDLE_USER_CONFIG=$XDG_CONFIG_HOME/bundle/config
+export BUNDLE_USER_PLUGIN=$XDG_DATA_HOME/bundle
